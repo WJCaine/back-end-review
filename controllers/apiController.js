cb517f646ba5743eb7128f42b60fdddf498e31be
Your Name <Billy.caine@gmail.com>
2019-10-11 14:20:29 +0100

    possibly fixed heroku

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,5 +1,6 @@
 const { selectEndpoints } = require("../models/apiModel");
+const { endpoints } = require("../endpoints.json");
 
 exports.sendEndpoints = (req, res, next) => {
-  res.sendFile("../endpoints.json");
+  res.status(200).send(endpoints);
 };
